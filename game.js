d36598b4120d8975bc4d642c1af17b31efc43898
miciks <micikahmetali@gmail.com>
2023-01-15 08:48:51 +0300

    start game

--- game.js
+++ game.js
@@ -4,3 +4,19 @@ let scoreText=document.getElementById("score")
 let questionText=document.getElementById("question")
 let choicesText= Array.from(document.getElementsByClassName("choice-text"))
 console.log(choicesText)
+
+let timeCounter=10
+let availableQuestion = []
+let currentQuestion = {}
+let score = 0
+let question = 0
+let acceptingAnswer = true
+const CORRECT_POINT = 100/questions.lenght
+const MAXIMUM_QUESTION = questions.length
+
+function startGame() {
+    score = 0
+    question = 0
+    availableQuestion=[...questions]
+    getNewQuestion()
+}
